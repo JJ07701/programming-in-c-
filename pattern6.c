93313387cddb9a9ba3523102af94789adb932c05
JJ07701 <jangraj845@gmail.com>
2025-09-13 20:39:06 +0530

    Create pattern6.c

---
+++ pattern6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n=5;
+     for(int i=5;i<=i;--i)
+  {    
+      for(int k=1;k<=5-i;++k)
+      {
+          printf(" ");
+      }
+      
+     
+      for( int j=1;j<=i;j++)
+      {
+           printf("* ");
+      }
+      printf("\n");
+         
+  }
+    return 0;
+}
+
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+
