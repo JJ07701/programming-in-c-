03243851e24d5d26553d59e6c65c1a3d2010488e
JJ07701 <jangraj845@gmail.com>
2025-09-13 20:51:14 +0530

    Create pattern14.c

---
+++ pattern14.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=(5-i);j++)
+        {
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++)
+        {   if(k==1||k==i||i==5)
+            printf(" *");
+            else 
+            printf("  ");
+        }
+        printf("\n");
+    }
+    
+return 0;
+}
+
+     *
+    * *
+   *   *
+  *     *
+ * * * * *
+
+
+
